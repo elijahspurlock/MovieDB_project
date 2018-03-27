c3194a64c79b3ecd68bed318cb5d75fcc484e5fe
Eli Spurlock <elijahspurlock@mail.weber.edu>
2018-03-27 10:32:53 -0600

    created delete option

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -36,6 +36,10 @@ class MoviesController < ApplicationController
     end
 
     def destroy
+    	@movie = Movie.find(params[:id])
+    	@movie.destroy
+
+    	redirect_to movies_path
     end
 
     private
