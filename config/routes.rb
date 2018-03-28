a0ee5896f651e650b799dd861e6e6982fe4f437b
Eli Spurlock <elijahspurlock@mail.weber.edu>
2018-03-28 14:44:30 -0600

    created user resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #get 'movies/mum'
 
   resources :movies 
-  
+  resources :users
   root 'website#index' # plug the website homepage / to use the route website/index defined above
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
